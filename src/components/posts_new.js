f0f02254aff5415f827c4dd77b48fb3f508a19db
Anthony Brown <abrown4123@gmail.com>
2018-08-02 18:04:29 -0500

    Added Cancel Button and Styled all a tags inside a form with a 5px left margin

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
   renderField(field) {
@@ -10,7 +11,7 @@ class PostsNew extends Component {
       <div className={className}>
         <label>{field.label}</label>
         <input
-          className='form-contol'
+          className='form-control'
           type='text'
           {...field.input}
         />
@@ -31,7 +32,7 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
-          label='title'
+          label='Title'
           name='title'
           component={this.renderField}
         />
@@ -46,6 +47,7 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <button type='submit' className='btn btn-primary'>Submit</button>
+        <Link className='btn btn-danger' to='/'>Cancel</Link>
       </form>
     );
   }
@@ -58,10 +60,10 @@ function validate(values) {
     errors.title = 'Enter a title!'
   }
   if(!values.categories) {
-    errors.title = 'Enter a category!'
+    errors.categories = 'Enter a category!'
   }
   if(!values.content) {
-    errors.title = 'Enter content!'
+    errors.content = 'Enter content!'
   }
   return errors;
 }
